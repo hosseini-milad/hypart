d6036cc43ec78e2d5f631de95f29d522632bd4e1
milad <milad.cia@gmail.com>
2024-05-12 06:59:29 +0330

    limit

--- router/panelProductApi.js
+++ router/panelProductApi.js
@@ -158,9 +158,8 @@ router.post('/fetch-product',jsonParser,async (req,res)=>{
         const productData = await ProductSchema.findOne({_id: ObjectID(productId)})
         const brandList = await BrandSchema.find({})
         const categoryList = await category.find({})
-        const brandData = brandList.find(item=>item.brandCode==productData.brandId)
         const catData = categoryList.find(item=>item.catCode==productData.catId)
-        const filterList = await Filters.find({"category._id":catData._id.toString()})
+        const filterList = catData&&await Filters.find({"category._id":catData._id.toString()})
        
         res.json({filter:productData,brandList:brandList,categoryList:categoryList,
         brandData:brandData,catData:catData,filterList:filterList})
